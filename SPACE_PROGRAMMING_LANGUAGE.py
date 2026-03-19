f6e4d2b5a300c328c1825d9ac59b0d3f3165f402
GaivenCCC <MrGaiven@proton.me>
2026-03-19 03:15:40 -0400

    ADD LOGO TO FILE

---
+++ SPACE_PROGRAMMING_LANGUAGE.py
@@ -0,0 +1,123 @@
+# SPACE_programming_langauge
+
+                                                        #                                 ---------
+                                                        #                               //         \\
+                                                        #      /---------------------/ //  -------  \\ /-------------------------/
+                                                        #     /--------------------/  //  /  ___  \  \\ /-----------------------/
+                                                        #    /---------------------/ ||  |  /___\  |  || /---------------------/
+                                                        #   /----------------------/ ||  |  \___/  |  || /--------------------/
+                                                        #                             \\  \_______/  //                      
+                                                        #                              \\           //
+                                                        #                               \\_________//
+           
+
+
+####################################################################################
+# DEVELOPER: CLAUDE G. REID III, CJ, GAIVEN, CLAUDE GAIVEN                        ##
+# DESCRIPTION: A programming language that defines spaces,                        ##
+#               realities, and atoms and any object/thing using space objects.    ##
+# DEVELOPMENT-DATE-AND-TIME-STAMP : 3/19/2026, 2:57 PM                            ##
+####################################################################################
+# COPYRIGHT 2026-
+
+
+
+# SPACE PROGRAMMING LANGUAGE
+
+# ADD PARTICLES TOGETHER TO FORM ANY THING.
+# DEFINE FIELD WITH PARTICLES
+# DEFINE DEFINE OBJECTS WITH FIELD-PARTICLES AND ADD THEM UP.
+
+# .space
+# can make the fex langauge.
+'''
+ATOM = MATTER + EXCITATION
+FUNCTOR = TRANSFORM + OBJECT
+PROGRAM = CODE + SPACE
+MACHINE = EVOLUTION + MATTER + SPACE
+SIGNAL = EXCITATION + FIELD + DIRECTION
+
+PARTICLE-OF-FIELD = ATOM + FUNCTOR + PROGRAM + MACHINE + SIGNAL
+
+HIGGS-PARTICLE = A PARTICLE OF A FIELD, THAT FIELD IS REALITY.
+
+GRAVITY = FORCE + FIELD + MATTER + MASS
+'''
+
+
+                                                       # types of development spaces
+    #                                                   ----------------------------
+    # string -> main_object ->  areaonics       -> computer_space(state_space) -> turing machine -> intelligent-machine -> actions
+    #                                           -> ? -> drawing_rules -> graphics -> animations -> magic -> .....
+    #                                           -> definitions(atom-layer-0) -> selection of ifs/decisions -> process -> algorithm -> programming_space -> programs -> actions
+    #                                           -> geometry(atom-L-0/machine-T-0) -> form(atom-L-1) -> magic -> light -> space -> time -> music -> ....
+    #                                           -> differential_equation(atom of maath_space)[ machine-type-0 ] -> math_space(topology) -> math_equations -> monoids -> functions -> functors -> categories ->  homograph -> ....
+    #                                           -> machines -> intelligent-machines -> thinking-machines -> .....
+                   # MATH                       -> number(atom-L-0) -> operation -> operator -> function -> equation -> formula -> proof -> reasoning -> .......
+ # ENGINEERING                                  -> discovering -> thinking -> plannings -> draftings -> designs -> systems -> tools -> constructors -> structures -> .....
+           #POWER-ENGINEERING                   -> space -] matter -]] electrons -> electricity => energy ->> space ->>> time ->>>> reality ->>>>> .... (-> ---> power, these are transformation functions/operations)
+              #CHEMISTRY                       #-> space -} atoms -}} molecules -}}} organelles -}}}} organism -}}}}}} ameoba -] cells -]] organs -]]] systems -]]]] biological-beings -\ evolving-beings -\ ......
+             # ELECTRICIANS/ETC.        #       -> @ -+ tools -+ knowledge -+ planing-out -+ creation -+ (build + repair) -+ maintenance -+ @/return-to-start-of-field -+ ....
+
+                                        
+    # these are the "particles-types" of each state.
+    # each state has a machine, programs, atoms, ....
+    # SYSTEMS = ATOMS
+    # JUST NEED TO DEFINE THE FUNCTOR AND THE SYSTEM/OBJECT
+    # ATOM AND FUNCTOR TO MAKE ANY FIELD, THEREFORE ANYTHING.
+    # ATOMS AND FUNCTOR AND PROGRAMS AND MACHINES AND ...., A WAND MAKES ALL OF THINGS.
+    # FIELD = [ATOM, FUNCTOR, PROGRAM, MACHINE, COMMUNICATION-ELEMENT(SIGNAL)]
+    
+    # FIELD = [ATOM, FUNCTOR, PROGRAM, MACHINE, SIGNAL], THESE 5 THINGS MAKE UP THE ELEMENTS OF A WAND.
+    
+    # OBJECT = [SIGNAL, ATOM]
+    # ROBOT = [ MACHINE, SIGNAL, PROGRAM ]
+    # CAR = [ MACHINE, SIGNAL, FUNCTOR ]
+    # CAR = [ ( STEERING-WHEEL, ENGINE, COMPUTER, WIPER-FLUID, WIPER, CHAIRS, .... ), SIGNAL, FUNCTOR ]
+
+    # ENGINE = [ SPARK-PLUG, PISTON-RING, CONNECTING-ROD, ENGINE-BLOCK, FLYWHEEL, HEAD-GASKET,
+            #        OIL-FILTER, DISTRUTOR, RADIATOR, EXHAUST-MANIFOLD, OIL-PUMP,
+            #        CRANKSHAFT-POSITION-SENSOR, VACUMM-PUMP, FUEL-PUMP, AIR-FILTER ]
+
+                    
+    # HOW-TO-CONSTRUCT-AN-ENGINE = BASE-CAR-SHELL -= CONNECTION-ROD -+ VACUMM-PUMP -> OIL-PUMP = OIL-FILTER ....
+
+# HELLO 
+
+# DEFINE THE OBJECT USING THE PARTICLES OF A REALITY.
+
+# THE PROGRAMMING ELEMENTS OF ANY OBJECT/THING.
+
+# DEFINE THE ATOM, can use any symbol to make this. O
+# DEFINE THE FUNCTOR, -O, a connection is graph theory.
+# DEFINE THE PROGRAM, [O]
+# DEFINE THE MACHINE, (O.)
+# DEFINE THE SIGNAL, (~O}
+
+# O is any symbol/object. the symbol connected to is a perminent definer.
+
+# + IS AN EXAMPLE OF A SIGNAL, 
+
+# A LAYER MUST HAVE ALL 5 ELEMENTS OF THE PARTICLE.
+
+A LINE IN THIS PROGRAMMING LANGAUGE IS A LAYER.
+
+A LAYER IS MADE UP OF FIELDS WHICH ARE MADE UP OF PARTICLES.
+
+LAYER = L1_[ATOM, FUNCTOR, PROGRAM, MACHINE, SIGNAL] + L2_[ATOM, FUNCTOR, PROGRAM, MACHINE, SIGNAL] + ..........
+
+
+
+# HOW DO YOU VISUALIZE THESE THINGS.
+
+DESIGN = CAR +| DONE = CAR COMPLETELY DONE
+
+# IF OBJECT IS UNDEFINED, THEN RETURN "UNDEFINED OBJECT" RETURN TYPE.
+
+# USE DEFINED REALITY ELEMENTS.
+A = ['CAR']
+
+# DEFINE THE FIELD
+# DEFINE THE SPACE
+# DEFINE THE REALITY
+# DEFINE THE OBJECT
